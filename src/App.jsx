ff7f5ea4c61ffb6cb8dcd73c5f2f7820cb7f039b
shanuchauhan1211 <shanu1211chauhan@gmail.com>
2024-08-30 16:02:26 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as  Router, Route, Routes } from "react-router-dom";
-import Home from './pages/home';
 import Login from './pages/Login';
+import Home from "./pages/Home";
 function App() {
   
 
