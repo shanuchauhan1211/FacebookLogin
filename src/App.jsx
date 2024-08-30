2a4c330efee3718c1c7ba379a0ef3b485b2007f5
shanuchauhan1211 <shanu1211chauhan@gmail.com>
2024-08-30 16:05:11 +0530

    login added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { BrowserRouter as  Router, Route, Routes } from "react-router-dom";
-import Login from './pages/Login';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 function App() {
-  
-
   return (
     <>
-
-     <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login />} />
-        
-      </Routes>
-    </Router>
-  </>
-  )
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
