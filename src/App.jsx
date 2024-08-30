53be221c06871809ca13b87c3f5d9b4d43145a0c
shanuchauhan1211 <shanu1211chauhan@gmail.com>
2024-08-30 16:10:25 +0530

    hello added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Hello from "./pages/Hello";
 function App() {
   return (
     <>
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/hello" element={<Hello />} />
         </Routes>
       </Router>
     </>
